86e49a5625d8168ffb0f948fc757f2f8f97a36c8
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-08-08 16:02:13 -0400

    Theme

--- Final R Script.R
+++ Final R Script.R
@@ -203,7 +203,7 @@ n + geom_histogram(binwidth = 10, aes(fill = Genre), color = "Black") + ylim(0, 
 
 # zoom
 
-n + geom_histogram(binwidth = 10, aes(fill = Genre), color = "Black") + 
+n + geom_histogram(binwidth = 10, aes(fill = Genre), color = "Black") +
   coord_cartesian(ylim = c(0, 50))
 
 # Improving w
@@ -212,5 +212,58 @@ w + geom_point(aes(size = BudgetMillions))  +
   facet_grid(Genre ~ Year) +
   coord_cartesian(ylim = c(0, 100))
 
+# *** Theme***
+x <- ggplot(data = movies, aes(x = BudgetMillions))
+h <-
+  x + geom_histogram(binwidth = 10, aes(fill = Genre), colour = "Black")
 
+# Axes labels
+h + xlab("Money Axis") + ylab("Number of Movies")
 
+# Label formatting
+h + xlab("Money Axis") + ylab("Number of Movies") +
+  theme(
+    axis.title.x = element_text(color = "DarkGreen", size = 30),
+    axis.title.y = element_text(color = "Red", size = 30)
+  )
+
+# Tick mark formatting
+h + xlab("Money Axis") + ylab("Number of Movies") +
+  theme(
+    axis.title.x = element_text(color = "DarkGreen", size = 30),
+    axis.title.y = element_text(color = "Red", size = 30),
+    axis.text.x = element_text(size = 20),
+    axis.text.y = element_text(size = 20)
+  )
+
+? theme
+
+# Legend formatting
+
+h + xlab("Money Axis") + ylab("Number of Movies") +
+  theme(
+    axis.title.x = element_text(color = "DarkGreen", size = 30),
+    axis.title.y = element_text(color = "Red", size = 30),
+    axis.text.x = element_text(size = 20),
+    axis.text.y = element_text(size = 20),
+    legend.title = element_text(size = 30),
+    legend.text = element_text(size = 20),
+    legend.position = c(1, 1),
+    legend.justification = c(1, 1)
+  )
+
+# Title
+h + xlab("Money Axis") +
+  ylab("Number of Movies") +
+  ggtitle("Movie Budget Distribtion") +
+  theme(
+    axis.title.x = element_text(color = "DarkGreen", size = 30),
+    axis.title.y = element_text(color = "Red", size = 30),
+    axis.text.x = element_text(size = 20),
+    axis.text.y = element_text(size = 20),
+    legend.title = element_text(size = 30),
+    legend.text = element_text(size = 20),
+    legend.position = c(1, 1),
+    legend.justification = c(1, 1),
+    plot.title = element_text(color="DarkBlue", size = 40, family = "Courier")
+  )
